17d5d2d19a3570f0097501fdf7d84ab802dbeca3
hidexir <hidexir@gmail.com>
2021-05-10 03:24:02 +0900

    WIP

--- src/main.rs
+++ src/main.rs
@@ -22,6 +22,9 @@ const EDI: usize = 7;
 const REGISTERS_COUNT: usize = 8;
 const REGISTERS_NAME: [&str; 8] = ["EAX", "ECX", "EDX", "EBX", "ESP", "EBP", "ESI", "EDI"];
 
+type InstFunc = fn(&mut Emulator);
+
+type Insts = [InstFunc; 256];
 
 fn type_of<T>(_: T) -> String{
   let a = std::any::type_name::<T>();
@@ -50,6 +53,25 @@ fn create_emu(eip: usize, esp: u32) -> Emulator {
     };
 } 
 
+fn nop(_emu: &mut Emulator) {
+}
+
+// MOV r32, imm32: Move imm32 to r32.
+fn mov_r32_imm32(emu: &mut Emulator) {
+    let reg: usize = (get_code8(emu, 0) - 0xB8).try_into().unwrap();
+    let value = get_code32(emu, 1);
+    emu.regs[reg] = value;
+    emu.eip += 5;
+}
+
+fn init_instructions(instructions: &mut Insts) {
+	for i in 0..8 {
+        instructions[0xB8 + i] = mov_r32_imm32;
+	}
+    instructions[0xEB] = short_jump;
+}
+
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     let mut emu = create_emu(0x0000, 0x7c00);
@@ -71,4 +93,21 @@ fn main() {
         },
     };
     let file_len = file.metadata().unwrap().len();
+	let mut binary = Vec::<u8>::new();
+
+    match file.read_to_end(&mut binary) {
+        Err(why) => panic!("couldn't read {}: {}", display,
+                                                   why.description()),
+        Ok(_) => println!("read file from {}\n", display),
+    }
+    emu.mem = binary;
+    let mut instructions: Insts = [nop; 256];
+    println!("{:?}",instructions.len());
+    println!("{:?}",instructions[0](&mut emu));
+    println!("{:p}", instructions[0] as *const());
+    init_instructions(emu);
+
+
+    println!("finish!! main()");
 }
+
